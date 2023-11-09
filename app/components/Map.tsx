44dd667d4b5c57fe67a79f0a028cf934b6a368f3
Boldoo <burnaa111@yahoo.com>
2023-11-10 03:56:18 +0800

    fix

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -7,14 +7,6 @@ import 'leaflet/dist/leaflet.css'
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import { useState } from 'react';
-import { useMapEvents } from 'react-leaflet';
-import { LatLng, LeafletEvent } from 'leaflet';
-import { Popup } from 'react-leaflet';
-
-const [position, setPosition] = useState<LatLng | null>(null as LatLng | null);
-
-
 
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl; 
@@ -31,33 +23,11 @@ interface MapProps {
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-function LocationMarker() {
-  const [position, setPosition] = useState<LatLng | null>(null);
-
-  // Explicitly define the type for the map object
-  const map = useMapEvents({
-    click(e: LeafletEvent) {
-      map.locate();
-    },
-    locationfound(e: LeafletEvent) {
-      const latlng = (e as any).latlng as LatLng; // Cast latlng to LatLng
-      setPosition(latlng);
-      map.flyTo(latlng, map.getZoom());
-    },
-  });
-
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
-  );
-}
-
 const Map: React.FC<MapProps> = ({ center }) => {
   return (
       <MapContainer 
-        center={center as L.LatLngExpression || [46, 105]} 
-        zoom={13} 
+        center={center as L.LatLngExpression || [51, -0.09]} 
+        zoom={center ? 4 : 2} 
         scrollWheelZoom={false} 
         className="h-[35vh] rounded-lg"
       >
